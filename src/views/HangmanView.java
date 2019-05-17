46a9f211971aea8a92d79536096a550aece53504
ori gilad <turhdhkgd@gmail.com>
2019-05-17 17:18:08 +0300

    add view components to frame

--- src/views/HangmanView.java
+++ src/views/HangmanView.java
@@ -3,6 +3,7 @@ package views;
 import Models.HangmanModel;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class HangmanView extends JFrame {
     private JLabel _wordToGuess;
@@ -12,8 +13,22 @@ public class HangmanView extends JFrame {
 
     public HangmanView(HangmanModel model) {
         _wordToGuess = new GameLabel(model.get_encryptedWord());
-        _chosenBank = new GameLabel("");
+        _chosenBank = new GameLabel("Letters Already used:");
         _lettersBank = new BankComboBox(model.get_lettersBank());
+        _image = new HangmanPanel(model.get_numOfBadGuesses());
 
+        setLayout(new BorderLayout());
+        add(_chosenBank, BorderLayout.PAGE_START);
+        add(_image, BorderLayout.LINE_START);
+        add(_lettersBank, BorderLayout.LINE_END);
+        add(_wordToGuess, BorderLayout.PAGE_END);
+
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setSize(new Dimension(500, 500));
+        setVisible(true);
+    }
+    
+    public void showError(String s) {
+        //TODO
     }
 }
