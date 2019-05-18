040926663809206a674b4c265f2c2d8bb2006741
ori gilad <turhdhkgd@gmail.com>
2019-05-18 21:38:58 +0300

    hangman panel painting and images

--- src/views/HangmanPanel.java
+++ src/views/HangmanPanel.java
@@ -1,10 +1,15 @@
 package views;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class HangmanPanel extends JPanel {
     private int _lines;
+    private BufferedImage image;
 
     public HangmanPanel(int numOfLines) {
         _lines = numOfLines;
@@ -21,5 +26,16 @@ public class HangmanPanel extends JPanel {
 
     public void addLine(){
         _lines++;
+        try {
+            image = ImageIO.read(new File("D:\\Repos\\Hangman\\src\\views\\" +_lines+".png"));
+        } catch (IOException ex) {
+            // handle exception...
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, this); // see javadoc for more info on the parameters
     }
 }
